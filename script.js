9f1c78750a81d95fb1c502273ec9d4079be23d55
Mayron Castillo <12mayron12@gmail.com>
2025-02-14 09:33:39 -0600

    Ajuste en una function

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const header = document.querySelector(".header");
 const color = document.querySelector('[type="color"]');
 const tamanio = document.querySelector('[type="range"]');
 
-color.addEventListener("input", function(e){    
+color.addEventListener("input", (e)=> {    
     const nuevoColor = e.target.value;
     header.style.color = nuevoColor;
 });
 
-tamanio.addEventListener("input", function(e){    
+tamanio.addEventListener("input", (e)=> {    
     const nuevoTamanio = e.target.value;
     header.style.fontSize = nuevoTamanio + "px";
 });
+
